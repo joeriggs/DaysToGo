9d0adcea2e2e79d05e1bee0603693508d09bd581
Joe Riggs <jriggs@Joes-iMac.local>
2017-06-24 18:00:41 -0400

    Formatting looks good (for now).
    Use UIDatePicker to select a date.  Display it with a decent format.

--- Days To Go/ViewController.swift
+++ Days To Go/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     var endTime = Date()
     var timer = Timer()
     let resultFormatter = DateComponentsFormatter()
+    let targetDateFormatter = DateFormatter()
 
     @IBOutlet weak var descLabel: UILabel!
     @IBOutlet weak var progressLabel: UILabel!
@@ -29,33 +30,29 @@ class ViewController: UIViewController {
     }
 
     @IBOutlet weak var datePickerValue: UIDatePicker!
-    @IBAction func datePickerTapped(_ sender: Any) {
-        self.endTime = datePickerValue.date
+    @IBAction func datePickerTapped(_ sender: UIDatePicker) {
+        endTime = datePickerValue.date
+        descLabel.text = targetDateFormatter.string(from: endTime)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
+        // Set targetDateFormatter to the format we use for displaying the target date.
+        targetDateFormatter.dateFormat = "dd/MM/yyyy H:mm a"
+        
         // Initialize our resultFormatter.  We use it for 2 different things:
         // 1. To calculate the difference between curTime and endTime.
-        // 2. To format the resut that we display on the screen.
+        // 2. To format the result that we display on the screen.
         resultFormatter.allowedUnits = [.day, .hour, .minute, .second]
         resultFormatter.unitsStyle = .abbreviated
 
         // Initialize endTime.  It contains our target timestamp.  This will change
         // as soon as we can save/restore dates.
-        if let userDate = String("2017/06/17 18:45") {
-            let formatter = DateFormatter()
-            formatter.dateFormat = "yyyy/MM/dd HH:mm"
-            if let targTime = formatter.date(from: userDate) {
-                self.endTime = targTime
-                descLabel.text = self.endTime.description
-                timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunc), userInfo: nil, repeats: true)
-            } else {
-                descLabel.text = "Invalid target \(userDate)"
-            }
-        }
+        endTime = datePickerValue.date
+        descLabel.text = targetDateFormatter.string(from: endTime)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunc), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
